dc4b1f946bddab85e49f622b2e384f94bdf48cd0
amirul12 <amirul.csejust@gmail.com>
2018-01-06 15:46:51 +0600

    Product Collection Resource -work in show products

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -20,7 +20,7 @@ class ProductResource extends Resource
               'price' => $this->price,
               'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
               'discount' =>$this->discount,
-              'totalPrice' => (1-($this->discount)/100)*$this->price,
+              'totalPrice' => round((1-($this->discount)/100)*$this->price,2),
               'rating' =>$this->reviews->count() >  0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Rating Yet',
               'href' =>[
                 'review'=>route('review.index',$this->id)
