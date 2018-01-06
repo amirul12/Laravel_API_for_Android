4bf19be09006f6489a8d03b8449e23d41f8b481f
amirul12 <amirul.csejust@gmail.com>
2018-01-06 09:43:43 +0600

    Modified product details rating and stock

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,15 @@ class ProductResource extends Resource
               'name' => $this->name,
               'description' => $this->detail,
               'price' => $this->price,
-              'stock' => $this->stock,
+              'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
               'discount' =>$this->discount,
+              'totalPrice' => (1-($this->discount)/100)*$this->price,
+              'rating' =>$this->reviews->count() >  0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Rating Yet',
+              'href' =>[
+                'review'=>route('review.index',$this->id)
+
+              ],
+            
         ];
     }
 }
