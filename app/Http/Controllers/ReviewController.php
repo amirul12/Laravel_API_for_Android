250077742daf2856b5cd55d9d462b6ac1dabd95c
amirul12 <amirul.csejust@gmail.com>
2018-01-07 09:36:37 +0600

    Review Resources and paginate

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Model\Review;
+use App\Model\Product;
+use App\Http\Resources\ReviewResource;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. 
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Product $product)
     {
-        //
+         
+         //return ReviewResource::collection($product->reviews);
+         return ReviewResource::collection($product->reviews);
     }
 
     /**
